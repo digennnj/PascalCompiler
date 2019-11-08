18cce3ef1f02842a3ffd66c8d27d938921cb24a4
luczajmc <luczajmc@ceclnx01-19.csi.miamioh.edu>
2019-11-08 09:12:17 -0500

    Report results by test case instead of by directory in unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 shopt -s nullglob
+shopt -s globstar
 good() { echo -n $'\033[32m'"$@"$'\033[m';}
 bad() { echo -n $'\033[31m'"$@"$'\033[m';}
 test_dirs=("$@")
-if [[ $# -lt 1 ]]; then test_dirs=(TestCases/*); fi
+if [[ $# -lt 1 ]]; then test_dirs=(TestCases/); fi
 for test_dir in "${test_dirs[@]}"; do
-    echo -n "asm: "
-    for asm_file in "${test_dir}/"*.asm.good; do
-        prefix="${asm_file%%.asm.good}"
-        name="${prefix##$test_dir/}"
-        p_file="${prefix}".p
-        test_file="${prefix}".asm
-        if [[ ! -f "$p_file" ]]; then echo -n "[skip ${name}] "; continue; fi
-        ./pascal "$p_file" &>/dev/null
-        diff "${asm_file}" "${test_file}" &>/dev/null && good "${name} " || bad "${name} "
-    done
-    echo
-    echo -n "exec: "
-    for out_file in "${test_dir}/"*.out.good; do
-        prefix="${out_file%%.out.good}"
+    find "${test_dir}" -name '*.asm.good' -o -name '*.out.good' | sed -E 's/\.(asm|out)\.good$//g' | sort -u | while read prefix; do
+        echo -n "${prefix}: "
         name="${prefix##$test_dir/}"
         in_file="${prefix}".in.good
         asm_file="${prefix}".asm.good
-        test_file="${prefix}".out
-        if [[ ! -f "$asm_file" ]]; then echo -n "[skip ${name}] "; continue; fi
-        if [[ -f "$in_file" ]]; then cat "$in_file" | ./asm.py "$asm_file" 2>/dev/null >"${test_file}";
-        else ./asm.py "$asm_file" 2>/dev/null | tee "${test_file}"; fi
-        diff "${out_file}" "${test_file}" &>/dev/null && good "${name} " || bad "${name} "
+        out_file="${prefix}".out.good
+        p_file="${prefix}".p
+        testasm_file="${prefix}".asm
+        testout_file="${prefix}".out
+
+        if [[ -f "$p_file" && -f "$asm_file" ]]; then
+            ./pascal "$p_file" &>/dev/null
+            diff "${asm_file}" "${testasm_file}" &>/dev/null && good "asm " || bad "asm "
+        fi
+
+        if [[ -f "$asm_file" && -f "$out_file" ]]; then
+            if [[ -f "$in_file" ]]; then cat "$in_file" | ./asm.py "$asm_file" 2>/dev/null >"${testout_file}";
+            else ./asm.py "$asm_file" 2>/dev/null | tee "${testout_file}"; fi
+            diff "${out_file}" "${testout_file}" &>/dev/null && good "out " || bad "out "
+        fi
+        echo
     done
-    echo
 done

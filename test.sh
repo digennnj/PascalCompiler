9eb4d562366d2364ebeef547c1181fdf60d80a04
luczajmc <luczajmc@ceclnx01-19.csi.miamioh.edu>
2019-11-08 22:47:24 -0500

    Show line numbers for .asm/.p files in test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 pfile="$1"; asmfile="${1%%.p}".asm; outfile="${1%%.p}".out
 echo "=== PASCAL" &&
-cat "$pfile" &&
+cat -n "$pfile" &&
 ./pascal "$pfile" &&
 echo "=== ASM" &&
-cat "$asmfile" &&
+cat -n "$asmfile" &&
 echo "=== exec" &&
 python3 asm.py "$asmfile" | tee "$outfile"
